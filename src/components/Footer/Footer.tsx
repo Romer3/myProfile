e7219e59e80339917cee1f9f517a4d52feb22072
Romer228 <romaherasymov@gmail.com>
2022-10-10 00:11:03 +0300

    1

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
   const currentYear = new Date().getFullYear();
 
   return (
-    <div className='w-screen bg-slate-800'>
+    <div className='w-screen bg-dark'>
           <p className='flex text-xs justify-center font-semibold text-slate-400'>
             © {currentYear} Roma Herasymov • All Rights Reserved
           </p>
